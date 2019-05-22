783c5b56e21fe61b1bcf9342e97fec26a5c7b6cb
AlbAngs <42752130+AlbAngs@users.noreply.github.com>
2019-05-21 22:58:00 -0500

    modelo reuniones

--- application/controllers/newReunion.php
+++ application/controllers/newReunion.php
@@ -15,10 +15,35 @@ class newReunion extends CI_Controller {
 			$this->load->view('navbar');
 			$this->load->view('new_reunion');
 			$this->load->view('footer');
-		
+
+			
 	}
 
-	
+	public function enviar_datos(){
+			//guardamos los datos del formulario
+          $titulo = $this->input->post('titulo');
+          $asunto = $this->input->post('asunto');
+          $fecha = $this->input->post('fecha');
+          $lugar = $this->input->post('lugar');	
+          $hora = $this->input->post('hora');
+          $estatus= 1;
+         
+
+			$this->load->view('headers');
+			$this->load->view('navbar');
+			$this->load->view('footer');
+			 $datos_reunion = array(
+            'titulo' => trim(addslashes($titulo)),
+            'asunto' =>  trim(addslashes($asunto)),
+            'fecha' =>  trim(addslashes($fecha)),
+            'lugar'=>	trim(addslashes($lugar)),	
+            'hora' =>  trim(addslashes($hora)),
+            'estatus'=> trim(addslashes($estatus))
+          );		
+           $sql=$this->Model_newReunion->get_datos($datos_reunion);
+		
+          //echo("titulo: ".$titulo."lugar: ".$lugar."fecha: ".$fecha."hora: ".$hora."asunto: ".$asunto);
+	}
 }
 
 ?>
--- application/views/new_reunion.php
+++ application/views/new_reunion.php
@@ -1,30 +1,23 @@
 
 <div class="shadow p-3 mb-5 bg-white rounded card offset-md-3" style="width: 40rem; margin-top: 60px;">
   <h4 class="text-center">NUEVA REUNIÓN</h4>
-  <form>
+  <form class="" action="<?php echo base_url();?>index.php/newReunion/enviar_datos" method="post">
   <div class="form-group">
-    <input type="email" class="form-control text-center" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Introduzca el título">
+    <input type="text" class="form-control text-center" id="titulo" name="titulo" aria-describedby="emailHelp" placeholder="Introduzca el título" required=""  value="<?php echo set_value('titulo'); ?>">
   </div>
    <div class="form-group">
-    <input type="email" class="form-control text-center" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Introduzca el lugar">
+    <input type="text" class="form-control text-center" id="lugar" aria-describedby="emailHelp" name="lugar" placeholder="Introduzca el lugar" required=""  value="<?php echo set_value('lugar'); ?>">
   </div>
    <div class="form-group">
-    <script type="text/javascript">
-            $(function () {
-                $('#datetimepicker1').datetimepicker();
-            });
-        </script>
-   <div class='input-group date' id='datetimepicker1'>
-                    <input type='text' class="form-control"  />
-                    <span class="input-group-addon">
-                        <span class="glyphicon glyphicon-calendar"></span>
-                    </span>
-                </div>
+    <input type="date" class="form-control text-center" id="fecha" name="fecha" aria-describedby="emailHelp" required=""  value="<?php echo set_value('fecha'); ?>">
+  </div>
+  <div class="form-group">
+    <input type="time" class="form-control text-center" id="hora" name="hora" aria-describedby="emailHelp" required=""  value="<?php echo set_value('hora'); ?>">
   </div>
   <div class="form-group">
     <label for="exampleFormControlTextarea1">Motivos de la reunión</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" required=""  name="asunto" value="<?php echo set_value('asunto'); ?>"></textarea>
   </div>
-  <button type="submit" class="btn btn-primary offset-md-9">Crear Reunión</button>
+  <input type="submit" class="btn btn-primary offset-md-9" value="Crear Reunión"></input>
 </form>
 </div>
