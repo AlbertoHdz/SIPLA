c77d07b18c35868bb958f5970ad054a594bad8d9
Albert Hdz <alberto_8565@hotmail.com>
2019-05-21 22:07:41 -0500

    cambio en estilo
    se cambio el estilo de la navbar

--- application/views/headers.php
+++ application/views/headers.php
@@ -4,7 +4,7 @@
     <title>Login - SIPLA</title>
     <link rel="stylesheet" href="<?php echo base_url("public/css/bootstrap.min.css")?>">
     <link rel="stylesheet" href="<?php echo base_url("public/css/style-login.css")?>">
-    <script src="<?php echo base_url("public/js/bootstrap.min.js")?>"></script>
     <script src="<?php echo base_url("public/js/jquery.js")?>"></script>
+    <script src="<?php echo base_url("public/js/bootstrap.min.js")?>"></script>
 </head>
 <body>
